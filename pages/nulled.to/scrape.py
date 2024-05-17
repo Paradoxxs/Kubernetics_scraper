8b27ca87ed204067eae99f32aca98e4bc4c4f06d
Paradoxxs <Aston.dk@gmail.com>
2024-05-17 10:22:21 +0200

    docker-composes and change nulled

--- pages/nulled.to/scrape.py
+++ pages/nulled.to/scrape.py
@@ -5,82 +5,104 @@ from bs4 import BeautifulSoup
 import re
 from requests_html import HTMLSession
 from elasticsearch import Elasticsearch
+from datetime import datetime, timedelta
 
-Session = HTMLSession()
+# Initialize session
+session = HTMLSession()
 
 # Elasticsearch connection
-es = Elasticsearch([{'scheme': 'http','host': 'localhost', 'port': 9200}])
+try:
+    es = Elasticsearch([{'scheme': 'http', 'host': 'elasticsearch', 'port': 9200}])
+    if not es.ping():
+        raise ValueError("Connection failed")
+except Exception as e:
+    print("Error connecting to Elasticsearch:", e)
 
-user_agents = [ 
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', 
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36', 
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36', 
-    'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148', 
+# User agents and pages to scrape
+user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
     'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Mobile Safari/537.36',
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
 ]
 
-urls = ["https://www.nulled.to/#!Home","https://www.nulled.to/#!Pentesting","https://www.nulled.to/#!Leaks", "https://www.nulled.to/#!Coding" , "https://www.nulled.to/#!Monetizing", "https://www.nulled.to/#!Marketplace",  ]
-pages = []
-
+pages = [
+    "https://www.nulled.to/#!Home",
+    "https://www.nulled.to/#!Pentesting",
+    "https://www.nulled.to/#!Leaks",
+    "https://www.nulled.to/#!Coding",
+    "https://www.nulled.to/#!Monetizing",
+    "https://www.nulled.to/#!Marketplace"
+]
+topics = []
 
 def save_to_db(data):
     # Index data into Elasticsearch
-    es.index(index="scraped_data", doc_type="_doc", body={"data": data})
+    try:
+        es.index(index="scraped_data", body={"data": data})
+    except Exception as e:
+        print("Error indexing data:", e)
 
 def scrape_forums(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    all_links = soup.find_all("a")
-    pattern = re.compile(r".*/forum/.*")
+    try:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        all_links = soup.find_all("a")
+        pattern = re.compile(r".*/forum/.*")
+        for link in all_links:
+            href = link.get("href")
+            if href and pattern.match(href):
+                topics.append(href)
+    except Exception as e:
+        print("Error scraping forums:", e)
 
-    # Filter and print the links that match the pattern
-    for link in all_links:
-        href = link.get("href")
-        if href and pattern.match(href):
-            pages.append(href)
-
-
-def scrape_topics(soup):
-    #print(soup)
-    topics = soup.find_all("tr",class_="__topic")
-    for topic in topics:
+def scrape_posts(soup):
+    posts = soup.find_all("tr", class_="__topic")
+    for post in posts:
         try:
-            url = topic.find("td",class_="col_f_content").find("a").get("href")
-            #print(url)
-            title = topic.find("td",class_="col_f_content").find("a").text.strip().split("\n")[0]
-            #print(title)
-            author = topic.find("td",class_="col_f_content").find("a", class_="url").text.strip()
-            #print(author)
-            user_id = topic.find("td",class_="col_f_content").find("a", class_="url").get("href").split("/")[-1]
-            #print(user_id)
-            timestamp = topic.find("td",class_="col_f_content").find("a",class_="topic_title highlight_unread").get("title").split("started")[-1].replace("-","")
-            #if timestamp contains word today, replace it with today's date with the following format 28 December 2022
+            url = post.find("td", class_="col_f_content").find("a").get("href")
+            title = post.find("td", class_="col_f_content").find("a").text.strip().split("\n")[0]
+            author = post.find("td", class_="col_f_content").find("a", class_="url").text.strip()
+            user_id = post.find("td", class_="col_f_content").find("a", class_="url").get("href").split("/")[-1]
+            timestamp = post.find("td", class_="col_f_content").find("a", class_="topic_title highlight_unread").get("title").split("started")[-1].strip()
+            
             if "Today" in timestamp:
-                timestamp = time.strftime("%d %B %Y")
-
-            if "Yesterday" in timestamp:
-                #add yesterday to timestamp
-                timestamp = time.strftime("%d-%B-%Y")
-
-
-            save_to_db([url,title,user_id,author,timestamp])
-            print(f"inserted {url}")
+                timestamp = datetime.now().strftime("%d %B %Y")
+            elif "Yesterday" in timestamp:
+                timestamp = (datetime.now() - timedelta(1)).strftime("%d %B %Y")
+            else:
+                timestamp = timestamp.replace("-", "")
+            
+            save_to_db({"url": url, "title": title, "user_id": user_id, "author": author, "timestamp": timestamp})
+            print(f"Inserted {url}")
         except Exception as e:
-            print(e)
+            print("Error scraping posts:", e)
 
-def scrape_forum_page(url):
-    user_agent = random.choice(user_agents) 
+def scrape_topics(page):
+    user_agent = random.choice(user_agents)
     headers = {'User-Agent': user_agent}
     try:
-        response = Session.get(url,headers=headers)
+        response = session.get(page, headers=headers)
         soup = BeautifulSoup(response.content, 'html.parser')
-        scrape_topics(soup)
-    except:
-        print("Possible cloudflare")
+        scrape_posts(soup)
+    except Exception as e:
+        print("Possible Cloudflare or scraping error:", e)
 
+print("Starting scraping")
 
-## update the select statement so it only gets the once that have not been scraped before.
 for page in pages:
-    scrape_forum_page(page)
-    time.sleep(5)
+    print(f"Scraping page: {page}")
+    scrape_forums(page)
+
+# Update the select statement so it only gets the ones that have not been scraped before
+for topic in topics:
+    print(f"Scraping topic: {topic}")
+    # Check if the topic has already been scraped by querying Elasticsearch
+    try:
+        if not es.exists(index="scraped_data", id=topic):
+            scrape_topics(topic)
+            time.sleep(5)
+    except Exception as e:
+        print("Error checking or scraping topic:", e)
